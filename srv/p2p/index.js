54686c6f755fcbc28ac1531ce2ba37552669c687
Jacob Friedman <jacob@solvent.ca>
2020-10-21 16:39:06 -0400

    update

--- srv/p2p/index.js
+++ srv/p2p/index.js
@@ -10,23 +10,21 @@ const NOISE 				= require('libp2p-noise')
 const MPLEX 				= require('libp2p-mplex')
 const Bootstrap 		= require('libp2p-bootstrap')
 const WebSockets 		= require('libp2p-websockets')
-const WebRTCStar		= require('libp2p-webrtc-star')
+const MulticastDNS	= require('libp2p-mdns')
 
 //////////////// 		LIBP2P Helpers
 const wrtc 					= require('wrtc')
 const multiaddr 		= require('multiaddr')
 
 
-//console.log(ws1)
-
 //////////////// INITIALIZE  ////////////////////////
 
 
-const transportKey = WebRTCStar.prototype[Symbol.toStringTag]
-
 const peers = [
-	'/ip4/3.96.201.235/tcp/9999/ws/p2p-webrtc-star/'
+	'/ip4/3.96.201.235/tcp/9999/wss/p2p-webrtc-star/'
 ]
+
+
 
 const main = async () => {
 
@@ -40,48 +38,43 @@ const main = async () => {
 
 				},
 				modules:        {
-						transport: [WebRTCStar, WebSockets],
+						transport: [WebSockets],
 						connEncryption: [NOISE],
 						streamMuxer: [MPLEX],
-						peerDiscovery: [Bootstrap]
+						//peerDiscovery: [Bootstrap]
 				},
 				config: {
-			    transport: {
-			      [transportKey]: {
-			        wrtc // You can use `wrtc` when running in Node.js
-			      }
-			    },
 				  peerDiscovery: {
 					  autoDial: true, // Auto connect to discovered peers (limited by ConnectionManager minConnections)
 						  // The `tag` property will be searched when creating the instance of your Peer Discovery service.
 						  [Bootstrap.tag]: {
 								enabled: true,
 								list: peers // provide array of multiaddrs
-						  }
+						  },
+	            [MulticastDNS.tag]: {
+				        interval: 1000,
+				        enabled: true
+				      }
 					}
 				}
 		})
 
-		// print out listening addresses
-		const advertiseAddrs = node.multiaddrs
-		console.log('libp2p is advertising the following addresses: ', advertiseAddrs)
-		const listenAddrs = node.transportManager.getAddrs()
-		console.log('libp2p is listening on the following addresses: ', listenAddrs)
-
-		node.connectionManager.on('peer:connect', (connection) => {
-				  console.log('Connection established to:', connection.remotePeer.toB58String())	// Emitted when a new connection has been created
-				})
-
-				node.on('peer:discovery', (peerId) => {
-				  // No need to dial, autoDial is on
-				  console.log('Discovered:', peerId.toB58String())
-				})
-
 		// start libp2p
 		let output = await node.start()
-		console.log('libp2p has started')
+		console.log('🌈 libp2p has started 🌈\n')
 
-
+	  // print out listening addresses
+	  console.log('listening on addresses:')
+	  node.multiaddrs.forEach(addr => {
+	    console.log(`${addr.toString()}/p2p/${node.peerId.toB58String()}`)
+	  })
+			
+		node.connectionManager.on('peer:connect', (connection) => {
+		  console.log('Connection established to:', connection.remotePeer.toB58String())	// Emitted when a new connection has been created
+		})
+		node.on('peer:discovery', (peerId) => {
+		  console.log('Discovered:', peerId.toB58String())
+		})
 
 		const stop = async () => {
 		// stop libp2p
