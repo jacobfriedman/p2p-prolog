b9cd0f8859b8520076302de8fd874d0c53866518
Jacob Friedman <friedjacob@gmail.com>
2020-11-21 22:15:15 -0500

    updated code

--- srv/prolog/server.pl
+++ srv/prolog/server.pl
@@ -3,17 +3,17 @@
 :- use_module(library(http/http_dispatch)).
 
 :- http_handler(root(ws),
-	http_upgrade_to_websocket(echo, []),
-	[spawn([])]).
+    http_upgrade_to_websocket(echo, []),
+    [spawn([])]).
 
 echo(WebSocket) :-
     ws_receive(WebSocket, Message),
     (   Message.opcode == close
     ->  true
     ;   string_concat('Hey, you said ', Message.data , MessageRes),
-    	ws_send(WebSocket, text(MessageRes)),
+        ws_send(WebSocket, text(MessageRes)),
         echo(WebSocket)
     ).
 
-:- initialization
-	http_server(http_dispatch, [port(8083)]).
+:- http_server(http_dispatch, [port(8083)]).
+
